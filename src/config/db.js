cd894dc271bdac4f71688c584f5e7bfaa8f9f426
RifqiIp <rifqiiqbalpratama@gmail.com>
2026-02-10 14:59:01 +0700

    edit db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,20 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "postgres123",
-  database: "task_db",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-// pool.on("connect", () => {
-//   console.log("✅ Database connected");
-// });
+pool.on("connect", () => {
+  console.log("✅ Database connected");
+});
 
-// pool.on("error", (err) => {
-//   console.error("❌ Database error", err);
-//   process.exit(1);
-// });
+pool.on("error", (err) => {
+  console.error("❌ Database error", err);
+  process.exit(1);
+});
 
 module.exports = pool;
